0e5df109a7fe3dfa5be252cbae9d69beec8dfbb4
Mugesh Rajaram <mugeshrajaram@gmail.com>
2017-02-03 11:31:07 +0530

    test3

---
+++ typescriptApp/typescriptApp/typescript/index.js
@@ -0,0 +1,11 @@
+function getName() {
+    var name = 'Mugesh';
+    console.log(name);
+    alert(name);
+}
+var clsNew1 = (function () {
+    function clsNew1() {
+    }
+    return clsNew1;
+}());
+//# sourceMappingURL=index.js.map
---
+++ typescriptApp/typescriptApp/typescript/index.ts
@@ -0,0 +1,9 @@
+﻿function getName(): void {
+    let name: string = 'Mugesh'
+    console.log(name)
+    alert(name)
+}
+
+class clsNew1 {
+
+}

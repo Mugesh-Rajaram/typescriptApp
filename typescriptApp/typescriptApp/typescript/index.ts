a395c8f37fbb67297c050e53b8a4624a874a0704
Mugesh Rajaram <mugeshrajaram@gmail.com>
2017-02-04 18:58:15 +0530

    newCommit

--- typescriptApp/typescriptApp/typescript/index.ts
+++ typescriptApp/typescriptApp/typescript/index.ts
@@ -1,9 +1,33 @@
-﻿function getName(): void {
-    let name: string = 'Mugesh'
-    console.log(name)
-    alert(name)
-}
+﻿//function getName(): void {
+//    let name: string = 'Mugesh'
+//    console.log(name)
+//    alert(name)
+//}
 
 class clsNew {
+        strName: Object;
+    //constructor() {
+    //    //this.strName['name'] = 'Mugesh'
 
+    //    //    age: '26',
+    //    //        address: 'Namakkal'
+    //    //};
+    //}
+    getName() {
+        console.log(this.strName[0])
+        alert(this.strName[1])
+    }
 }
+
+function f(condition, x) {
+    
+    if (condition) {
+        let x = 100;
+        return x;
+    }
+
+    return x;
+}
+
+f(false, 0); // returns '0'
+f(true, 0);  // returns '100'
